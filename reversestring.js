2603d4b733616009e8b16f331a6b56cd1a09188a
Benjamin Huang <benjaminthuang@gmail.com>
2017-07-01 00:28:19 -0700

    reverse string recursion

--- reversestring.js
+++ reversestring.js
@@ -14,13 +14,13 @@
 
 
 // for loop 
-function reverseString(str) {
-    var newString = "";
-    for (var i = str.length - 1; i >= 0; i--) {
-        newString += str[i];
-    }
-    return newString;
-}
+// function reverseString(str) {
+//     var newString = "";
+//     for (var i = str.length - 1; i >= 0; i--) {
+//         newString += str[i];
+//     }
+//     return newString;
+// }
 
 // recursion 
 function reverseString(str){
